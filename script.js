e5b5ed9972c13392c28a3e79d2366a6852647e68
Bxxst_ <83009888+Bxxst@users.noreply.github.com>
2023-02-28 22:23:24 +0100

    logo pointer

---
+++ script.js
@@ -0,0 +1,10 @@
+const logoimg = document.getElementById("logo");
+logoimg.addEventListener("click", () => {
+    window.open("/index.html", "_self");
+})
+
+const btn = document.querySelector("#click-menu");
+btn.addEventListener('click', () => {
+    var menu = document.querySelector("#nav");
+
+})
